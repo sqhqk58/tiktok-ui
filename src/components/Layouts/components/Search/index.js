68b9f281d74c0d8cad1ecf4944cffaea20644fb8
sqhqk58 <sqhqk58@gmail.com>
2023-01-08 20:31:00 +0700

    fixx

--- src/components/Layouts/components/Search/index.js
+++ src/components/Layouts/components/Search/index.js
@@ -1,7 +1,7 @@
 import Tippy from '@tippyjs/react/headless'; // different import path!
 import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import AccountItem from '~/components/AccountItem';
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { wrapper as PopperWrapper} from '~/components/popper'
 import styles from '~/components/Layouts/components/Search/Search.module.scss';
 import classNames from 'classnames/bind';
@@ -15,38 +15,41 @@ function Search() {
         },0)
     })
     return (  
-        {/* Search */}
-        <Tippy
-            interactive
-            visible={searchResult.length>0}
-            render={attrs=>(
-                <div className={cx('search-results')} tabIndex="-1" {...attrs}>
-                    <PopperWrapper>
-                        <h4 className={cx('search-title')}>Account</h4>
-                        <AccountItem/>
-                        <AccountItem/>
-                        <AccountItem/>
-                        <AccountItem/>
-                    </PopperWrapper>
-                    </div>
-            )}
-        >
-            <div className={cx('search')}>
-                <input 
-                    placeholder='Search account and videos' 
-                    spellCheck={false}
-                />
-                <button className={cx('clear')}>
-                    <FontAwesomeIcon icon={faCircleXmark} />
-                </button>
-                <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-                
-                    <button className={cx('search-btn')}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+        <Fragment>
+            {/* Search */}
+            <Tippy
+                interactive
+                visible={searchResult.length>0}
+                render={attrs=>(
+                    <div className={cx('search-results')} tabIndex="-1" {...attrs}>
+                        <PopperWrapper>
+                            <h4 className={cx('search-title')}>Account</h4>
+                            <AccountItem/>
+                            <AccountItem/>
+                            <AccountItem/>
+                            <AccountItem/>
+                        </PopperWrapper>
+                        </div>
+                )}
+            >
+                <div className={cx('search')}>
+                    <input 
+                        placeholder='Search account and videos' 
+                        spellCheck={false}
+                    />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
                     </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
 
-            </div>
-        </Tippy>
+                </div>
+            </Tippy>
+        </Fragment>
+        
     );
 }
 
